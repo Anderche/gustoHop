72c8787e45a97044acf9b5b2751b5615d64d5897
Anders <kiss7medical@gmail.com>
2020-04-19 13:32:21 -0400

    add producer index view

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,2 +1,38 @@
 class ProducersController < ApplicationController
+
+  def index
+    @producers = Producer.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def producer_params
+    params.require(:producer).permit(:producer_name)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  resources :islands, only: [ :index, :show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: 'pages#home'
+
+  resources :islands, only: [ :index, :show ]
+
+  resources :producers
+
 end
