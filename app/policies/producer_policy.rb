1ed6f8b29da3449097b28bb08e1271ccb38a3438
Anders <kiss7medical@gmail.com>
2020-05-08 21:48:06 -0400

    add admin to users.rb

--- app/policies/producer_policy.rb
+++ app/policies/producer_policy.rb
@@ -14,14 +14,14 @@ class ProducerPolicy < ApplicationPolicy
   end
 
   def update?
-    user_is_owner?
+    user_is_owner_or_admin?
   end
 
   def destroy?
-    user_is_owner?
+    user_is_owner_or_admin?
   end
 
-  def user_is_owner?
-    record.user == user
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
   end
 end
